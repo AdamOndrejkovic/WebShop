a0e90b1bde452a0a6d5a9e23314b4e98d40e7d09
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2021-11-02 09:58:07 +0100

    Init

---
+++ WebShop.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebShop.WebApi.Dtos.Auth;
+
+namespace WebShop.WebApi.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpPost]
+        public ActionResult<TokenDto> Login([FromBody] LoginDto login)
+        {
+            if ("admin".Equals(login.Username) && "123456".Equals(login.Password))
+            {
+                //Get Token
+                return Ok(new TokenDto{JwtValue = "jwt12345"});
+            }
+            return Unauthorized();
+        }
+    }
+}
---
+++ WebShop.WebApi/Dtos/Auth/TokenDto.cs
@@ -0,0 +1,7 @@
+﻿namespace WebShop.WebApi.Dtos.Auth
+{
+    public class TokenDto
+    {
+        public string JwtValue { get; set; }
+    }
+}
