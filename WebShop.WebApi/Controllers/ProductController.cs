96f92d8c81e21bffa5eb56a53d85f983dc1066bc
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2021-11-04 10:25:29 +0100

    Product Controller

---
+++ WebShop.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebShop.Core;
+using WebShop.Core.IServices;
+
+namespace WebShop.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetOwners()
+        {
+            var products = _productService.GetOwners();
+            if (products != null)
+            {
+                //Ask about
+                return new ActionResult<IEnumerable<Product>>(products);
+            }
+
+            return BadRequest("No owners were found");
+        }
+    }
+}
